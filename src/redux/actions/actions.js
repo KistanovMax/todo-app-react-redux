2365bf0e2ef33fd3cd98c41d6d477ac9497e8c9c
Maxim Kistanov <maxim.kistanov@gmail.com>
2020-11-15 21:04:14 +0300

    added flags done / important to redux

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -8,6 +8,17 @@ export const deleteTodo = (todoId) => ({
   payload: todoId,
 });
 
+export const todoDone = (todoId, isTodoDone) => ({
+  type: 'TODO_DONE',
+  payload: { todoId, isTodoDone },
+});
+
+export const todoImportant = (todoId, isTodoImportant) => ({
+  type: 'TODO_IMPORTANT',
+  payload: { todoId, isTodoImportant },
+});
+
+
 export const transferDate = (currentDate) => ({
   type: 'TRANSFER_DATE',
   payload: currentDate,
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -4,9 +4,34 @@ const todoReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case 'ADD_TODO':
-      return (state = [...state, { id: payload.todoId, title: payload.title }]);
+      return (state = [
+        ...state,
+        {
+          id: payload.todoId,
+          title: payload.title,
+          isDone: false,
+          isImportant: false,
+        },
+      ]);
+
     case 'DELETE_TODO':
-      return (state = [...state.filter((todo) => todo.id !== payload)]);
+      return (state = [
+        ...state.filter((todo) => todo.id !== payload),
+      ]);
+
+    case 'TODO_DONE':
+      return state.map((todo) =>
+        todo.id === payload.todoId
+          ? { ...todo, isDone: !payload.isTodoDone }
+          : todo
+      );
+
+    case 'TODO_IMPORTANT':
+      return state.map((todo) =>
+        todo.id === payload.todoId
+          ? { ...todo, isImportant: !payload.isTodoImportant }
+          : todo
+      );
     default:
       return state;
   }
